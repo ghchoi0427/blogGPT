b9829a4da03c061a71858984048dc9bf77fa3edd
ghchoi <ghchoi0427@gmail.com>
2023-03-26 10:42:53 +0900

    feat: scheduledExecutorService

--- src/main/java/org/example/batch/BlogProgram.java
+++ src/main/java/org/example/batch/BlogProgram.java
@@ -7,28 +7,33 @@ import org.example.utils.HtmlUtil;
 import org.example.utils.Topics;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class BlogProgram {
 
     private final ChatApi chatApi;
     private final ImageApi imageApi;
     private final ExecutorService executorService;
+    private final ScheduledExecutorService scheduledExecutorService;
 
-    String[] topics = Topics.getTopics();
+    List<String> topics = Topics.getTopics();
     int index = 0;
 
     public BlogProgram(ChatApi chatApi, ImageApi imageApi, int numThreads) {
         this.chatApi = chatApi;
         this.imageApi = imageApi;
         this.executorService = Executors.newFixedThreadPool(numThreads);
+        this.scheduledExecutorService = Executors.newScheduledThreadPool(1);
+    }
+
+    public void start() {
+        scheduledExecutorService.scheduleAtFixedRate(this::writeBlog, 0, 24 * 60 * 60 / 15, TimeUnit.SECONDS);
     }
 
     public void writeBlog() {
-        String topic = topics[(index++) % topics.length];
+        System.out.println("upload initiated");
+        String topic = topics.get((index++) % topics.size());
         Future<String> imageUrlFuture = executorService.submit(() -> imageApi.run(topic));
         Future<String> contentFuture = executorService.submit(() -> chatApi.run(topic, 1000));
 
@@ -42,7 +47,7 @@ public class BlogProgram {
             throw new RuntimeException(e);
         }
 
-        String htmlContent = HtmlUtil.htmlFormatter(topic, imageUrl, content);
+        String htmlContent = HtmlUtil.htmlFormatter(imageUrl, content);
         try {
             BlogWriter.upload(topic, htmlContent);
         } catch (IOException | InterruptedException e) {
@@ -50,11 +55,12 @@ public class BlogProgram {
             throw new RuntimeException(e);
         } finally {
             shutdown();
+            System.out.println("writeBlog ended");
         }
     }
 
     public void shutdown() {
+        scheduledExecutorService.shutdown();
         executorService.shutdown();
     }
-
 }
